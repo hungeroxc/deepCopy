4f9412982b3c75bbd142861fd0d38f23168d4afd
dachunchun <524112857@qq.com>
2018-06-13 18:10:55 +0800

    up

---
+++ index.js
@@ -0,0 +1,3 @@
+const obj = require('./test.js')
+
+console.log(obj)
---
+++ test.js
@@ -0,0 +1,18 @@
+const test = {
+    num: 0,
+    str: '',
+    boolean: true,
+    unf: undefined,
+    nul: null,
+    obj: {
+        name: '我是一个对象',
+        id: 1
+    },
+    arr: [0, 1, 2],
+    func: function() {
+        console.log('我是一个函数')
+    },
+    date: new Date(0),
+    reg: new RegExp('/我是一个正则/ig'),
+    err: new Error('我是一个错误')
+}
